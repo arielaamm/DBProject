0e012109fc22fb835411cff5d2195f6bd2d1c143
Ariel Moreshet <arialaamm@gmail.com>
2024-07-24 11:54:53 +0300

    Update View1.sql

--- שלב ד/View1.sql
+++ שלב ד/View1.sql
@@ -11,3 +11,9 @@ SELECT c.customerid, c.name AS customer_name, o.orderid, o.totalamount
 FROM customers_view c
 JOIN air_order_view o ON c.customerid = o.customerid
 WHERE o.totalamount > 1000;
+
+-- Query 2: הצגת הזמנות שבוצעו בין תאריכים מסוימים
+SELECT o.orderid, o.orderdate, o.deliverydate, o.status, o.totalamount, c.customerid, c.name AS customer_name
+FROM air_order_view o
+JOIN customers_view c ON o.customerid = c.customerid
+WHERE o.orderdate BETWEEN '2024-01-01' AND '2024-06-30';
